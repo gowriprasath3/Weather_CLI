7fe351b7ea121328eb0138166723e7b840ee5535
gowriprasath3 <gowriprasath01@gmail.com>
2025-07-10 08:32:29 +0530

    first commit

---
+++ cmd/current.go
@@ -0,0 +1,34 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/go-resty/resty/v2"
+	"github.com/spf13/cobra"
+)
+
+var city string
+
+var currentCmd = &cobra.Command{
+	Use:   "current",
+	Short: "get the current weather",
+	Run: func(cmd *cobra.Command, args []string) {
+		const API_KEY = "07604ddd4e0764404025c9e293c62868"
+		// loc := args[0]
+		log.Print("getting weather for the location: ", city)
+		client := resty.New()
+		url := fmt.Sprintf("https://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s", city, API_KEY)
+		resp, err := client.R().Get(url)
+		if err != nil {
+			log.Fatalf("Request failed: %v", err)
+		}
+		fmt.Println("Response:", resp.String())
+	},
+}
+
+func init() {
+	log.Println("init current cmd...")
+	currentCmd.Flags().StringVarP(&city, "city", "c", "", "City to fetch weather for")
+	currentCmd.MarkFlagRequired("city")
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "weather-cli/cmd"
+
+func main() {
+	cmd.Execute()
+}
